49a54b7678b7d304495311b37010f65343333691
Li <malp0403@gmail.com>
2021-05-28 13:38:36 -0400

    aa

--- v0528/Controllers/WeatherForecastController.cs
+++ v0528/Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace v0528.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+           private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
